f64a17b2237105fd67729804a1f9bb9fe64d8dda
Анастасия Нестеренко <nesterenko.anastasia1996@gmail.com>
2022-01-19 21:55:02 +0700

    'правки_кода'

--- homework.py
+++ homework.py
@@ -1,10 +1,14 @@
+from http import HTTPStatus
+
+import json
+
 import logging
 
 import os
 
-import requests
-
 import time
+
+import requests
 
 import telegram
 
@@ -18,6 +22,7 @@ TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_STATUSES = {
@@ -39,10 +44,11 @@ handler.setFormatter(formatter)
 
 def send_message(bot, message):
     """Бот оправляет сообщение о статусе работы."""
-    if bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message):
-        pass
-    else:
-        raise Exception('Сообщение не отправленно')
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logger.info('Сообщение отправленно')
+    except telegram.TelegramError:
+        logger.error('Сообщение не отправлено')
 
 
 def get_api_answer(current_timestamp):
@@ -53,12 +59,19 @@ def get_api_answer(current_timestamp):
     """
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    if response.status_code == 200:
-        response = response.json()
-        return response
-    else:
-        raise Exception('Недоступность сайта в переменной ENDPOINT')
+
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+        if response.status_code == HTTPStatus.OK:
+            try:
+                response = response.json()
+                return response
+            except json.decoder.JSONDecodeError:
+                raise Exception('Запрос не преобразовался в формат JSON')
+        else:
+            raise Exception('Код доступа не равен 200')
+    except requests.ConnectionError:
+        raise Exception('Сайт недоступен')
 
 
 def check_response(response):
@@ -66,35 +79,33 @@ def check_response(response):
     Бот проверяет правильность запроса.
     Существования полей в словаре запроса.
     """
-    if response['homeworks'] \
-            or response['homeworks'] == [] \
-            and response['current_date']:
-        homeworks = response['homeworks']
-        homework = homeworks[0]
-        return homework
-    else:
-        raise Exception('Недоступнойсть ключей')
+    if not isinstance(response, dict):
+        raise TypeError('"response" не является словарем')
+    if 'homeworks' not in response:
+        raise TypeError('Ключ "homeworks" отсутсвует в "response"')
+    if 'current_date' not in response:
+        raise TypeError('Ключ "current_date" отсутсвует в "response"')
+    homeworks = response['homeworks']
+    homework = homeworks[0]
+    return homework
 
 
 def parse_status(homework):
     """Бот возвращает статус работы."""
-    if 'homework_name' in homework:
-        if 'status' in homework:
-            if homework['status'] in HOMEWORK_STATUSES:
-                homework_name = homework['homework_name']
-                homework_status = homework['status']
-                verdict = HOMEWORK_STATUSES[homework_status]
-                return f'Изменился статус проверки ' \
-                       f'работы "{homework_name}". {verdict}'
-            else:
-                raise Exception(
-                    'Статус домашней работы отсутсвует '
-                    'в словаре HOMEWORK_STATUSES'
-                )
-        else:
-            raise Exception('Отсутствует status домашней работы в запросе')
-    else:
-        raise KeyError('Отсутствует homework_name домашней работы')
+    if 'homework_name' not in homework:
+        raise KeyError('Отсутствует коюч "homework_name" в словаре')
+    if 'status' not in homework:
+        raise KeyError('Отсутствует ключ "status" в словаре')
+    if homework['status'] not in HOMEWORK_STATUSES:
+        raise Exception(
+            'Статус домашней работы '
+            'отсутсвует в словаре HOMEWORK_STATUSES'
+        )
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    verdict = HOMEWORK_STATUSES[homework_status]
+    return (f'Изменился статус проверки '
+            f'работы "{homework_name}". {verdict}')
 
 
 def check_tokens():
@@ -109,7 +120,10 @@ def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    check_tokens()
+    if check_tokens():
+        pass
+    else:
+        logger.critical('Отсутствуют переменные окружения')
     status = ''
     old_message = ''
     while True:
@@ -120,7 +134,7 @@ def main():
                 if homework['status'] != status:
                     message = parse_status(homework)
                     send_message(bot, message)
-                    logger.info('Сообщение отправленно')
+
                 else:
                     logger.debug('Отсутсие изменения статуса')
                 status = homework['status']
@@ -133,8 +147,6 @@ def main():
                 send_message(bot, message)
             old_message = message
             time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
